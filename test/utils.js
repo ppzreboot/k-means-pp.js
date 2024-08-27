c330f392533948b6ca46af3aa716f3435d862391
PPz <ppzreboot@qq.com>
2024-08-27 22:19:06 +0800

    test(utils): find_min()

--- test/utils.js
+++ test/utils.js
@@ -1,6 +1,6 @@
 import test from 'node:test'
 import assert from 'node:assert'
-import { distance } from '../utils.js'
+import { distance, find_min } from '../utils.js'
 
 test('distance between (0, 3) and (4, 0)', () => {
   assert.strictEqual(distance(2, [0, 3], [4, 0]), 5)
@@ -13,4 +13,15 @@ test('distance between (3, 0, 0) and (0, 4, 0)', () => {
   assert.strictEqual(distance(3, [3, 0, 0], [0, 4, 0]), 5)
   assert.strictEqual(distance(3, [3, 0, 0], [0, 0, 4]), 5)
   assert.strictEqual(distance(3, [3, 0, 0], [4, 0, 0]), 1)
+})
+
+test('find_min', () => {
+  assert.deepStrictEqual(
+    find_min([0,1,2,3]),
+    [0, 0],
+  )
+  assert.deepStrictEqual(
+    find_min([233, 100, 1, 3]),
+    [2, 1],
+  )
 })
