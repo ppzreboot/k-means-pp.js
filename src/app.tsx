4f5c57daf363615776bd912c170ccc278ae7a155
PPz <ppzreboot@qq.com>
2024-09-05 16:27:35 +0800

    feat: app

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,17 @@
+import { useState } from 'react'
+import { K_means_pp_demo } from './demo/ui'
+
 export
 function App() {
-  return <div>app</div>
+  const [file, set_file] = useState<File>()
+  return <div>
+    <h1>k-means-pp.js demo</h1>
+    {file
+      ? <K_means_pp_demo file={file} />
+      : <input
+        type='file'
+        onChange={evt => set_file(evt.target.files![0])}
+      />
+    }
+  </div>
 }
---
+++ src/demo/ui/index.tsx
@@ -0,0 +1,8 @@
+interface Props {
+  file: File
+}
+
+export
+function K_means_pp_demo(props: Props) {
+  return <div>k-means++</div>
+}
